10fbdd53b5fd99362b75e4cd4fac11a12b04705a
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2021-01-08 21:52:23 -0400

    Entity mapping

--- src/main/java/com/linkedin/CatalogItem.java
+++ src/main/java/com/linkedin/CatalogItem.java
@@ -1,25 +1,41 @@
 package com.linkedin;
 
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CATALOG_ITEM")
 public class CatalogItem {
 
+    @Id
+    @Column(name = "CATALOG_ITEM_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long itemId;
 
+    @Column(name = "NAME")
     private String name;
 
+    @Column(name = "MANUFACTURER")
     private String manufacturer;
 
+    @Column(name = "DESCRIPTION")
     private String description;
 
+    @Column(name = "AVAILABLE_DATE")
     private String availableDate;
 
     public CatalogItem() {
 
     }
 
-    public CatalogItem(Long itemId, String name, String manufacturer, String description, String availableDate) {
+    public CatalogItem(String name, String manufacturer, String description, String availableDate) {
         super();
-        this.itemId = itemId;
         this.name = name;
         this.manufacturer = manufacturer;
         this.description = description;
