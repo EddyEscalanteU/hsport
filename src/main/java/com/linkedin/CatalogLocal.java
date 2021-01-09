a2851ce641d864ba2ef2c55c522e44d3db91744a
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2021-01-08 22:06:48 -0400

    Advanced entity management

--- src/main/java/com/linkedin/CatalogLocal.java
+++ src/main/java/com/linkedin/CatalogLocal.java
@@ -8,6 +8,12 @@ import javax.ejb.Local;
 public interface CatalogLocal {
 
 	public List<CatalogItem> getItems();
-	
+
 	public void addItem(CatalogItem item);
+
+	public CatalogItem findItem(Long itemId);
+
+	public void deleteItem(CatalogItem item);
+
+	public List<CatalogItem> searchByName(String name);
 }
